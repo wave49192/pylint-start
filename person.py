dd2ef11d7a68e86133b86755aa4603f9512ff1ac
wave49192 <sirawich.tu@ku.th>
2021-10-04 11:36:55 +0700

    refactor code

--- person.py
+++ person.py
@@ -1,7 +1,10 @@
-#TODO Run pylint on this code. Fix all the problems until you get a score of 10.
-import logging
-from datetime import date, datetime
+
+"""person class"""
+from datetime import date
+
 today = date.today()
+
+
 class Person:
     """a person with a name and a birthday
     Example:
@@ -23,50 +26,64 @@ class Person:
     """
 
     def __init__(self, name, birthday=date.today()):
-        if not isinstance(name,str):
+        if not isinstance(name, str):
             raise TypeError("name must be a string")
         self._name = name
         self.birthday = birthday
 
     @staticmethod
-    def youthfulness(p):
+    def youthfulness(person):
         """Comment based on a person's age"""
-
-        if p.age < 0: return "unborn"
-        elif p.age < 1: return "baby"
-        elif p.age < 6: return "child"
-        elif p.age < 13: return "youth"
-        elif p.age < 20: return "teenager"
-        elif p.age < 30: return "young adult"
-        elif p.age < 60: return "middle-ager"
-        elif p.age < 80: return "senior"
-        elif p.age < 90: return "octogenarian"  # someone 80-89
-        elif p.age < 90: return "nonagenarian"  # someone 90-99
+        ans = 'none'
+        if person.age < 0:
+            ans = "unborn"
+        elif person.age < 1:
+            ans = "baby"
+        elif person.age < 6:
+            ans = "child"
+        elif person.age < 13:
+            ans = "youth"
+        elif person.age < 20:
+            ans = "teenager"
+        elif person.age < 30:
+            ans = "young adult"
+        elif person.age < 60:
+            ans = "middle-ager"
+        elif person.age < 80:
+            ans = "senior"
+        elif person.age < 90:
+            ans = "octogenarian"  # someone 80-89
+        elif person.age < 100:
+            ans = "nonagenarian"  # someone 90-99
         else:
-            return "centenarian"            # someone >= 100, aka "centarian"
-    
+            ans = "centenarian"  # someone >= 100, aka "centarian"
+        return ans
+
     def __str__(self):
-        return Person.youthfulness(self)+ " named " + self._name
+        return Person.youthfulness(self) + " named " + self._name
 
     def set_birthday(self, year: int, month: int, day: int):
+        """set birthday"""
         self.birthday = date(year, month, day)
 
     @property
     def age(self) -> int:
-        b = self.birthday
-        age = today.year - b.year
-        if (b.month,b.day) > (today.month,today.day):
+        """return age"""
+        birthday = self.birthday
+        age = today.year - birthday.year
+        if (birthday.month, birthday.day) > (today.month, today.day):
             # this year's birthday has not occurred yet
             age -= 1
         return age
 
     @property
     def name(self) -> str:
+        """return name"""
         return self._name
 
     def __eq__(self, o):
         """Two people are equal if they have the same name and birthday"""
         if not isinstance(o, Person):
             return False
-        else:
-            return self._name == o._name and self.birthday == o.birthday
+
+        return self._name == o._name and self.birthday == o.birthday
